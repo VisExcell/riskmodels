a53aa7bdb25ddbc08c474007efdbef7150b9c53a
Steve Moore <srmoore@gmail.com>
2016-06-17 13:51:44 -0400

    Changed convert_to_percent to roundLikeNCI   This does rounding like the NCI does in the GAIL code.

--- utils.py
+++ utils.py
@@ -1,10 +1,10 @@
 import numpy as np
 
 
-def convert_to_percent(np_float64):
+def roundLikeNCI(np_float64):
     outval = np.float64(np_float64) * np.float64(1000.0)
     if outval - outval.astype(np.int) >= np.float(0.5):
         outval = outval.astype(np.int) + 1
     else:
         outval = outval.astype(np.int)
-    return np.float(outval) / np.float(10)
+    return np.float(outval) / np.float(1000)
